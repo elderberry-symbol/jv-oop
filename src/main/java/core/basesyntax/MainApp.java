fc420bc16285177a5c644471bb951f44936c3fd4
Vaeltaja <>
2026-04-07 15:50:40 +0300

    initial task version, up and running

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck wheels = new Truck();
+        Bulldozer treads = new Bulldozer();
+        Excavator pillars = new Excavator();
 
+        Machine[] vehicles = {wheels, treads, pillars};
+        for (Machine vehicle : vehicles) {
+            vehicle.doWork();
+            vehicle.stopWork();
+        }
+    }
 }
